5047a563fd1bad6e69ef873b0917881753948926
Raluca <ralucam.sandu@gmail.com>
2017-10-11 15:32:08 +0200

    add view for patient

--- liverdata/models.py
+++ liverdata/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     patient_name = models.CharField(max_length=20)
-    birth_date = models.DateTimeField('birthdate')
+    #birth_date = models.DateTimeField('birthdate')
 
     def __str__(self):
         return self.patient_name
--- liverdata/views.py
+++ liverdata/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Patient
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>hello there homepage</h1>")
+    all_patients = Patient.objects.all()
+    html = ''
+    for patient in all_patients:
+        url = '/liverdata/' + str(patient.id) + '/'
+        html += '<a href="' + url + '" >' + patient.patient_name + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request,patient_id):
+    return HttpResponse("<h2>Details for Patient_ID: " + str(patient_id) + "</h2>")
