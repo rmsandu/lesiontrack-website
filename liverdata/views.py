03b4d9ed502d72c4e6566d75cc15c242bfed010f
Raluca <ralucam.sandu@gmail.com>
2017-10-11 16:16:01 +0200

    add http404 error

--- liverdata/views.py
+++ liverdata/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from .models import Patient
 # Create your views here.
 
 def index(request):
     all_patients = Patient.objects.all()
-    context = {'all_patients' : all_patients}
-    return render(request, 'liverdata/index.html', context)
+    #context = {'all_patients' : all_patients}
+    return render(request, 'liverdata/index.html', {'all_patients' : all_patients})
 
 def detail(request,patient_id):
-    return HttpResponse("<h2>Details for Patient_ID: " + str(patient_id) + "</h2>")
+    try:
+        patient = Patient.objects.get(pk=patient_id)
+    except Patient.DoesNotExist:
+        raise Http404("Patient does not exist")
+    return render(request, 'liverdata/detail.html', {'patient' : patient})
