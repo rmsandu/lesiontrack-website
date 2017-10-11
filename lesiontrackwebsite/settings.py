24141ac933bddc5d8ba7cd4b58bdc8ecbd385763
Raluca <ralucam.sandu@gmail.com>
2017-10-11 11:28:28 +0200

    add classes for patient, lesion, treatment

--- lesiontrackwebsite/settings.py
+++ lesiontrackwebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'liverdata.apps.LiverdataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- liverdata/models.py
+++ liverdata/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    patient_name = models.CharField(max_length=20)
+    #birth_data = models.DateTimeField('birthdate')
+
+    def __str__(self):
+        return self.patient_name
+
+
+class Lesion(models.Model):
+    patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
+    lesion_name = models.CharField(max_length=250) # change to Choice Field
+    # lesion_type = do we need this??
+    lesion_size = models.IntegerField(default=0)
+    lesion_biopsy = models.CharField(max_length=1) # choice field, y/n
+
+    def __str__(self):
+        return self.lesion_name
+
+
+class Treatment(models.Model):
+    lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
+    treatment_name = models.CharField(max_length=10) # rfa, mwa
+    treatment_type = models.CharField(max_length=10) # intervention, laparoscopy, open
+    treatment_date = models.DateTimeField('treatment date')
