c3a6038f60983cf4286d2c8e51f084dded6fd817
Raluca <ralucam.sandu@gmail.com>
2017-10-11 15:05:00 +0200

    add admin

--- liverdata/admin.py
+++ liverdata/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Patient, Treatment, Lesion
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Lesion)
+admin.site.register(Treatment)
