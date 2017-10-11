ca641df8076f681a8cc19e09dda3ecfae7aa3d11
Raluca <ralucam.sandu@gmail.com>
2017-10-11 15:46:22 +0200

    add templates folder

--- liverdata/views.py
+++ liverdata/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Patient
 # Create your views here.
 
 def index(request):
     all_patients = Patient.objects.all()
-    html = ''
-    for patient in all_patients:
-        url = '/liverdata/' + str(patient.id) + '/'
-        html += '<a href="' + url + '" >' + patient.patient_name + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('liverdata/index.html')
+    # create dictionary for template
+    context = {
+        'all_patients' : all_patients,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request,patient_id):
     return HttpResponse("<h2>Details for Patient_ID: " + str(patient_id) + "</h2>")
