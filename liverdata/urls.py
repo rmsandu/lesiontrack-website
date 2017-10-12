76201dcd794e595743e153b3546803bb8e1049e6
Raluca <ralucam.sandu@gmail.com>
2017-10-12 10:52:18 +0200

    remove hardcoded url index page

--- liverdata/urls.py
+++ liverdata/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /liverdata/
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<patient_id>[0-9]+)/$', views.detail, name='lesion_detail'),
-    url(r'^(?P<patient_id>[0-9]+)/(?P<lesion_id>[0-9]+)/$', views.detail1, name='treatment_detail')
+    # /liverdata/713/
+    url(r'^(?P<patient_id>[0-9]+)/$', views.detail, name='patientdetail'),
+    # /liverdata/patientxyx/lesionxyz/
+    url(r'^(?P<patient_id>[0-9]+)/(?P<lesion_id>[0-9]+)/$', views.detail1, name='lesiondetail')
 ]
