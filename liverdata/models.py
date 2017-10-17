3e0a1051d3186f535b96433ebc25077d325f76df
Raluca <ralucam.sandu@gmail.com>
2017-10-17 09:30:40 +0200

    add choices in the model treatment

--- liverdata/models.py
+++ liverdata/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     patient_name = models.CharField(max_length=20)
-    #birth_date = models.DateTimeField('birthdate')
+    # study_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.patient_name
@@ -12,18 +12,26 @@ class Patient(models.Model):
 class Lesion(models.Model):
     patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
     lesion_name = models.CharField(max_length=250) # change to Choice Field
-    lesion_size = models.IntegerField(default=0)
-    lesion_biopsy = models.CharField(max_length=1) # choice field, y/n
+    lesion_size = models.DecimalField(max_digits=10, decimal_places=2)
+    lesion_biopsy = models.BooleanField(default=False) # choice field, y/n
 
     def __str__(self):
-        return self.lesion_name
+        return  'Patient Name: ' + str(self.patient) + '; Lesion: ' + self.lesion_name
 
 class Treatment(models.Model):
     lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
-    #patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    treatment_name = models.CharField(max_length=10) # Change to Choice Field rfa, mwa
+    # patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     treatment_type = models.CharField(max_length=10) # intervention, laparoscopy, open
     treatment_date = models.DateTimeField('treatment date')
+    treatment_name = models.CharField(choices=(
+                ('RFA', "Radio-frequency Ablation"),
+                ('MWA', "Microwave Ablation"),
+                ('IRE', "Irreversible electroporration"),
+                ('R', "Resection"),
+                ('T',"TACE"),
+                ('C', "Chemotherapy")
+            ),
+            max_length=3)
 
     def __str__(self):
         return self.treatment_name
