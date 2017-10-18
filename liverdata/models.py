efd42762dbbb64aceb3f7c04acd2afbcbb96d5e6
Raluca <ralucam.sandu@gmail.com>
2017-10-18 09:44:49 +0200

    new models

--- liverdata/models.py
+++ liverdata/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     patient_name = models.CharField(max_length=20)
-    # study_name = models.CharField(max_length=100)
+    gender = models.CharField(choices=(('F','Female'), ('M','Male')), max_length=1, default='F')
+    study_site = models.CharField(max_length=100, default='Bern')
+    study_name = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.patient_name
@@ -18,20 +20,62 @@ class Lesion(models.Model):
     def __str__(self):
         return  'Patient Name: ' + str(self.patient) + '; Lesion: ' + self.lesion_name
 
-class Treatment(models.Model):
-    lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
-    # patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    treatment_type = models.CharField(max_length=10) # intervention, laparoscopy, open
-    treatment_date = models.DateTimeField('treatment date')
-    treatment_name = models.CharField(choices=(
-                ('RFA', "Radio-frequency Ablation"),
-                ('MWA', "Microwave Ablation"),
-                ('IRE', "Irreversible electroporration"),
-                ('R', "Resection"),
-                ('T',"TACE"),
-                ('C', "Chemotherapy")
-            ),
-            max_length=3)
+class TreatmentSession(models.Model):
+    patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
+    treatmentsession_date = models.DateTimeField('Treatment Session Date:')
+    treatment_nr = models.PositiveIntegerField()
+    treatment_type = models.CharField(choices=(
+        ('IR', 'Intervention'),
+        ('LP', 'Laparoscopy'),
+        ('Open', 'Surgery'),
+        ('Resection', 'Resection'),
+        ('Chemotherapy', 'Chemotherapy')
+        ),
+        max_length=10) # intervention, laparoscopy, open, Resection
 
     def __str__(self):
-        return self.treatment_name
+        return 'Patient:' + str(self.patient) + '; TreatmentSession: ' + self.treatment_nr
+
+class Treatment(models.Model):
+    lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
+    treatmentsession = models.ForeignKey(TreatmentSession, on_delete = models.CASCADE)
+    treatment_name = models.CharField(max_length=100) # Chemotherapy,resection,embolization
+
+    def __str__(self):
+        return 'Patient:' + str(self.patient) + '; TreatmentSession: ' + self.treatmentsession
+
+class Device(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    needle_name = models.CharField(max_length=200)
+    others = models.TextField()
+
+    def __str__(self):
+        return 'Device Name: ' + self.manufacturer + 'Needle_Name:' + self.needle_name
+
+
+class MWA(models.Model):
+    treatment = models.ForeignKey(Treatment, blank=True, null=True, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, blank=True, null=True, on_delete=models.CASCADE)
+    power = models.PositiveIntegerField() # power or temperature applied
+    time_duration = models.DurationField()
+
+    def __str__(self):
+        return 'TreatmentName:' + MWA.__name__
+
+class RFA(models.Model):
+    treatment = models.ForeignKey(Treatment, blank=True, null=True, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, blank=True, null=True, on_delete=models.CASCADE)
+    power = models.PositiveIntegerField() # power or temperature applied
+    time_duration = models.DurationField()
+
+    def __str__(self):
+        return 'Treamtent Name:' + RFA.__name__
+
+class IRE(models.Model):
+    treatment = models.ForeignKey(Treatment, blank=True, null=True, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, blank=True, null=True, on_delete=models.CASCADE)
+    power = models.PositiveIntegerField() # power or temperature applied
+    time_duration = models.DurationField()
+
+    def __str__(self):
+        return 'TreatmentName:' +  IRE.__name__
