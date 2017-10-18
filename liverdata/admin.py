412df7b0f411afa74b525ac82fc5446d15ce22c8
Raluca <ralucam.sandu@gmail.com>
2017-10-18 11:03:11 +0200

    recreated the whole database

--- liverdata/admin.py
+++ liverdata/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Patient, Treatment, Lesion
+from .models import *
 
 # Register your models here.
 admin.site.register(Patient)
 admin.site.register(Lesion)
 admin.site.register(Treatment)
+admin.site.register(TreatmentSession)
+admin.site.register(Device)
+admin.site.register(RFA)
+admin.site.register(IRE)
+admin.site.register(MWA)
+admin.site.register(TreatmentName)
+admin.site.register(LesionType)
--- liverdata/models.py
+++ liverdata/models.py
@@ -10,15 +10,21 @@ class Patient(models.Model):
     def __str__(self):
         return self.patient_name
 
+class LesionType(models.Model):
+    lesiontype_name = models.CharField(max_length=50)
+    lesiontype_shortname = models.CharField(max_length=5)
+
+    def __str__(self):
+        return "{0} ({1})".format(self.lesiontype_name, self.lesiontype_shortname)
 
 class Lesion(models.Model):
     patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
-    lesion_name = models.CharField(max_length=250) # change to Choice Field
     lesion_size = models.DecimalField(max_digits=10, decimal_places=2)
     lesion_biopsy = models.BooleanField(default=False) # choice field, y/n
+    lesiontype = models.ForeignKey(LesionType, on_delete = models.PROTECT, null=True, blank=False)
 
     def __str__(self):
-        return  'Patient Name: ' + str(self.patient) + '; Lesion: ' + self.lesion_name
+        return  'Patient Name: ' + str(self.patient) + '; Lesion: ' + str(self.lesiontype)
 
 class TreatmentSession(models.Model):
     patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
@@ -34,15 +40,22 @@ class TreatmentSession(models.Model):
         max_length=10) # intervention, laparoscopy, open, Resection
 
     def __str__(self):
-        return 'Patient:' + str(self.patient) + '; TreatmentSession: ' + self.treatment_nr
+        return 'Patient:' + str(self.patient) + '; TreatmentSession: ' + str(self.treatment_nr)
 
-class Treatment(models.Model):
-    lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
-    treatmentsession = models.ForeignKey(TreatmentSession, on_delete = models.CASCADE)
-    treatment_name = models.CharField(max_length=100) # Chemotherapy,resection,embolization
+class TreatmentName(models.Model):
+    treatment_name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=5)
 
     def __str__(self):
-        return 'Patient:' + str(self.patient) + '; TreatmentSession: ' + self.treatmentsession
+        return self.short_name
+
+class Treatment(models.Model):
+    treatmentsession = models.ForeignKey(TreatmentSession, on_delete = models.CASCADE)
+    lesion = models.ForeignKey(Lesion, on_delete=models.CASCADE)
+    treatmentname = models.ForeignKey(TreatmentName, on_delete = models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'Lesion:' + str(self.lesion) + '; TreatmentSession: ' + str(self.treatmentsession)
 
 class Device(models.Model):
     manufacturer = models.CharField(max_length=200)
@@ -62,6 +75,9 @@ class MWA(models.Model):
     def __str__(self):
         return 'TreatmentName:' + MWA.__name__
 
+    class Meta:
+        verbose_name_plural = "MWA"
+
 class RFA(models.Model):
     treatment = models.ForeignKey(Treatment, blank=True, null=True, on_delete=models.CASCADE)
     device = models.ForeignKey(Device, blank=True, null=True, on_delete=models.CASCADE)
@@ -71,6 +87,9 @@ class RFA(models.Model):
     def __str__(self):
         return 'Treamtent Name:' + RFA.__name__
 
+    class Meta:
+        verbose_name_plural = "RFA"
+
 class IRE(models.Model):
     treatment = models.ForeignKey(Treatment, blank=True, null=True, on_delete=models.CASCADE)
     device = models.ForeignKey(Device, blank=True, null=True, on_delete=models.CASCADE)
@@ -79,3 +98,6 @@ class IRE(models.Model):
 
     def __str__(self):
         return 'TreatmentName:' +  IRE.__name__
+
+    class Meta:
+        verbose_name_plural = "IRE"
